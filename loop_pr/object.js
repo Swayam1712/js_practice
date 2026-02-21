e23127cdd9a905a852c5961eacda45e2fb48cd2f
Swayam sankalpa Panigrahi <swayamsankalpapanigrahi@gmail.com>
2026-02-21 21:49:50 +0530

    ....

---
+++ loop_pr/object.js
@@ -0,0 +1,35 @@
+// // 
+// const info={
+//     swayam:{
+//         grade:"a",
+//         city:"bbsr"
+//     },
+//     anshul:{
+//         grade:"b",
+//         city:"delhi"  
+//     },
+//     rahul:{
+//         grade:"c",
+//         city:"mumbai"
+//     }  
+// };
+ 
+// 
+const info=[
+        {
+            name:"swayam",
+        grade:"a",
+        city:"bbsr"
+    },
+    { 
+        name:"anshul",
+        grade:"b",
+        city:"delhi"  
+    },
+    { 
+        name:"rahul",
+        grade:"c",
+        city:"mumbai"
+    }  
+];
+ 
