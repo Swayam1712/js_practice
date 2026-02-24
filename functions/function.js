4c65b041cc47380072583fbea4c45f192f024a41
Swayam sankalpa Panigrahi <swayamsankalpapanigrahi@gmail.com>
2026-02-24 23:56:34 +0530

    ...

--- functions/function.js
+++ functions/function.js
@@ -4,3 +4,44 @@ function diceRoll() {
 }
 
 diceRoll();
+
+
+function Sum(a,b){
+    return a+b;
+
+}
+let s= Sum(3,4);
+console.log(s);
+
+  function multiplication(a){
+    for(let i=1;
+        i<=10; i++){
+       console.log(a*i);
+        }
+    
+  }
+    multiplication(5);
+
+
+
+    function sumOfnNo(n){
+        let sum=0;
+        for (let i=1;i<=n ; i++){
+            sum=sum+i;
+        }
+    return sum;
+    }
+     console.log(sumOfnNo(50));
+
+
+
+     let str=["hello","world","javascript","functions"];
+     function concat(str){
+       let result ="";
+         for(let i=0; i<str.length; i++){
+             result+=str[i];
+     }
+     return result;
+    }
+
+    console.log(concat(str));
