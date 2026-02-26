6d74f9c04e58b012d9c362cc4f24c26f2bb3eebd
Swayam sankalpa Panigrahi <swayamsankalpapanigrahi@gmail.com>
2026-02-26 23:18:47 +0530

    ..

--- functions/function.js
+++ functions/function.js
@@ -58,3 +58,12 @@ console.log(s);
     inerArea();
 }
    outer();
+
+function factorial(n){
+    if(n==0|| n==1){
+        return 1;
+     } else {
+            return n*factorial(n-1);
+        }
+    }
+   console.log(factorial(7));
