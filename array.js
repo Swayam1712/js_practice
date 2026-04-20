3b5de585b836d09d7ab9e63794919ad79251bd54
Swayam sankalpa Panigrahi <swayamsankalpapanigrahi@gmail.com>
2026-04-20 21:48:59 +0530

    ...

--- array.js
+++ array.js
@@ -36,8 +36,11 @@ console.log(first);
 //let arr=["1","2","3","4","5","6","7","8","9","10"];
  //arr.splice(n);
    // console.log(arr);
+
+   
    
    /* for(let i=1 ;i<=10;i++)
     {
         console.log(i);
     }*/
+
